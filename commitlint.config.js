44b4bd0993ec3503d4191b72ca0bbd0f75ade032
AlexGorSer <taskekaminari@gmail.com>
2026-05-10 16:07:11 +0200

    docs: add new docs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional', '@commitlint/config-angular'],
   rules: {
+    'header-max-length': [2, 'always', 200],
     'type-enum': [
       2,
       'always',
