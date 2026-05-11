aec01d2ef02d46ff61f490d9d46e34a7875b6e21
AlexGorSer <taskekaminari@gmail.com>
2026-05-12 01:39:58 +0200

    chore: update docs, scripts, add shared, rename app.components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,13 @@ import { provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideTaiga()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideTaiga(),
+    provideStore(),
+  ],
 };
