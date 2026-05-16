5e8b1d388ac9a198b68d869435a39a142553f8a3
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-16 21:09:31 +0700

    feat: add about page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import type { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+// import { AboutPageComponent } from './components/features/about-page/about-page.component';
+
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     loadComponent: () => import('./components/features/about-page/about-page.component').then((m) => m.AboutPageComponent),
+//   },
+// ]
---
+++ src/app/components/features/about-page/about-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'left-paw-about-page',
+  imports: [],
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutPageComponent {}
