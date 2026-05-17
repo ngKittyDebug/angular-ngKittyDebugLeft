4056c6f39a34e2593231315c9fb9799cd1160cc0
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-17 14:07:06 +0700

    feat(about): add layout for page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import type { Routes } from '@angular/router';
-
-export const routes: Routes = [];
 // import { AboutPageComponent } from './components/features/about-page/about-page.component';
 
-// export const routes: Routes = [
-//   {
-//     path: '',
-//     loadComponent: () => import('./components/features/about-page/about-page.component').then((m) => m.AboutPageComponent),
-//   },
-// ]
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadComponent: () => import('./components/features/about-page/about-page.component').then((m) => m.AboutPageComponent),
+  // },
+];
--- src/app/components/features/about-page/about-page.component.ts
+++ src/app/components/features/about-page/about-page.component.ts
@@ -1,10 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
+import { TuiAccordion } from '@taiga-ui/kit';
 
 @Component({
   selector: 'left-paw-about-page',
-  imports: [],
+  imports: [KeyValuePipe, TuiAccordion],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  protected readonly data = {
+    'Основные технологии': `Framework: Angular v21
+      State Management: NgRx (@ngrx/store)
+      UI Library: Taiga UI v5
+      Language: TypeScript ~5.9
+      Reactive Programming: RxJS`,
+    Тестирование: `Vitest`,
+    'Code Quality': `ESLint
+        Prettier
+       Stylelint`,
+    'Git Workflow & Automation': `Husky
+commitlint
+lint-staged`,
+    'В проекте используются строгие правила': `Conventional Commits
+валидация названий веток
+автоматические проверки перед commit`,
+  };
+}
