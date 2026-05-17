d2485ce4a4829bf79ddaa78bc0bba9b7da525b27
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-17 14:07:02 +0300

    feat: add all styles. add another gif image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import type { Routes } from '@angular/router';
+import { NotFoundPageComponent } from '@features/not-found/ui/components/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
--- src/app/features/not-found/ui/components/not-found-page/not-found-page.component.ts
+++ src/app/features/not-found/ui/components/not-found-page/not-found-page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'left-paw-not-found-page',
-  imports: [],
+  imports: [TuiButton],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
