f0856872dfb4ec6cbf410d0a8a9d3a06262e845e
AlexGorSer <taskekaminari@gmail.com>
2026-05-17 17:02:47 +0200

    feat(header): add header compoinent, add pokemon svg icon

--- src/app/core/ui/components/layout/header/header.component.ts
+++ src/app/core/ui/components/layout/header/header.component.ts
@@ -1,10 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { TuiLink } from '@taiga-ui/core';
+
+const POKEMON_ICON_SRC = 'images/svg/pokeball.svg';
 
 @Component({
   selector: 'left-paw-header',
-  imports: [],
+  imports: [TuiLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public readonly pokeDexIcoSrc = input<string>(POKEMON_ICON_SRC);
+}
--- src/app/core/ui/components/layout/layout.component.ts
+++ src/app/core/ui/components/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'left-paw-layout',
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
