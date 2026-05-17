94b8488994a34c0ceaf9f29971e358664b2278d4
AlexGorSer <taskekaminari@gmail.com>
2026-05-17 22:45:08 +0200

    feat(header): add buttons settings

--- src/app/core/ui/components/layout/header/header.component.ts
+++ src/app/core/ui/components/layout/header/header.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { TuiIcon, TuiLink } from '@taiga-ui/core';
+import { TuiButton, TuiIcon, TuiLink } from '@taiga-ui/core';
 import type { NavListItem } from './model/nav-list-items';
 
 const POKEMON_ICON_SRC = 'images/svg/pokeball.svg';
 
 @Component({
   selector: 'left-paw-header',
-  imports: [TuiLink, TuiIcon],
+  imports: [TuiLink, TuiIcon, TuiButton, TuiIcon],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
