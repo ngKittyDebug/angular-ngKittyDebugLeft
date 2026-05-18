f0bd51d14cd40a790a2cdbf66e1c4c2e73cf0032
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-18 09:37:39 +0300

    refactor: change button tag to a tag. change home button styles

--- src/app/features/not-found/ui/components/not-found-page/not-found-page.component.ts
+++ src/app/features/not-found/ui/components/not-found-page/not-found-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'left-paw-not-found-page',
-  imports: [TuiButton],
+  imports: [TuiButton, RouterLink],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
