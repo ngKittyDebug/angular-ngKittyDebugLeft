d820823a881865c00bf92a46e0c3af0b0d505036
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-18 22:59:16 +0700

    refactor(routes): delete commented code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import type { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  // {
-  //   path: 'about',
-  //   loadComponent: () =>
-  //     import('./components/features/about-page/about-page.component').then(
-  //       (m) => m.AboutPageComponent,
-  //     ),
-  // },
-];
+export const routes: Routes = [];
