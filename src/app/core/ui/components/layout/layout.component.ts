73cfe488dda4593587fe6508a62eef71db07aa16
AlexGorSer <taskekaminari@gmail.com>
2026-05-20 21:50:29 +0200

    feat(footer): add footer component

--- src/app/core/ui/components/layout/layout.component.ts
+++ src/app/core/ui/components/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { NAV_LIST_ITEMS } from './constans/nav-list';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'left-paw-layout',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
