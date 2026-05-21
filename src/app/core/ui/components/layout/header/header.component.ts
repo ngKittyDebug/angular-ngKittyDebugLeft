10c48db5cf156a7174fca428059a6903e9d6291b
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-22 00:21:16 +0700

    feat(i18n): bind language switcher to header button

--- src/app/core/ui/components/layout/header/header.component.ts
+++ src/app/core/ui/components/layout/header/header.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { TuiButton, TuiIcon, TuiLink } from '@taiga-ui/core';
 import type { NavListItem } from '../model/nav-list-items';
+import { LanguageSwitcher } from '@core/services/transloco.service';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 const POKEMON_ICON_SRC = 'images/svg/pokeball.svg';
 
 @Component({
   selector: 'left-paw-header',
-  imports: [TuiLink, TuiIcon, TuiButton, TuiIcon],
+  imports: [TuiLink, TuiIcon, TuiButton, TuiIcon, TranslocoDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,4 +16,5 @@ const POKEMON_ICON_SRC = 'images/svg/pokeball.svg';
 export class HeaderComponent {
   public readonly navListItems = input<NavListItem[]>([]);
   protected readonly pokeDexIcoSrc = POKEMON_ICON_SRC;
+  protected language = inject(LanguageSwitcher);
 }
