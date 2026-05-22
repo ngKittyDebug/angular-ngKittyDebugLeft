d1be960295c18e3cf43a4348cc1dfcdbd699c074
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-23 00:32:28 +0700

    feat(i18n): add transloco to routing and add folders for translating

--- src/app/core/ui/components/layout/header/header.component.ts
+++ src/app/core/ui/components/layout/header/header.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core
 import { TuiButton, TuiIcon, TuiLink } from '@taiga-ui/core';
 import type { NavListItem } from '../model/nav-list-items';
 import { LanguageSwitcher } from '@core/services/transloco.service';
-import { TranslocoDirective } from '@jsverse/transloco';
+import { provideTranslocoScope, TranslocoDirective } from '@jsverse/transloco';
 
 const POKEMON_ICON_SRC = 'images/svg/pokeball.svg';
 
@@ -12,6 +12,7 @@ const POKEMON_ICON_SRC = 'images/svg/pokeball.svg';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideTranslocoScope('header')],
 })
 export class HeaderComponent {
   public readonly navListItems = input<NavListItem[]>([]);
--- src/app/features/about/ui/components/about-page/about-page.component.ts
+++ src/app/features/about/ui/components/about-page/about-page.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { KeyValuePipe } from '@angular/common';
 import { TuiAccordion } from '@taiga-ui/kit';
 import { accordionData } from './constants/accordion-constants';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'left-paw-about-page',
-  imports: [KeyValuePipe, TuiAccordion],
+  imports: [KeyValuePipe, TuiAccordion, TranslocoDirective],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
