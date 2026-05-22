8bd48e7285f02fb75183352e8e848f9a4ca71411
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-22 15:43:06 +0300

    feat: add changing theme icons abillity

--- src/app/core/services/theme-switcher.service.ts
+++ src/app/core/services/theme-switcher.service.ts
@@ -1,16 +1,31 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 
 export type AppTheme = 'light' | 'dark';
+
+const AppThemeNames = {
+  light: 'light',
+  dark: 'dark',
+} as const;
+
+const AppThemeIcons = {
+  light: '@tui.sun',
+  dark: '@tui.moon',
+} as const;
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeSwitcherService {
-  public readonly currentTheme = signal<AppTheme>('dark');
+  public readonly currentTheme = signal<AppTheme>(AppThemeNames.dark);
+
+  public readonly themeIcon = computed(() => {
+    return this.currentTheme() === AppThemeNames.light ? AppThemeIcons.light : AppThemeIcons.dark;
+  });
 
   public toggleTheme(): void {
     this.currentTheme.update((theme) => {
-      const nextTheme: AppTheme = theme === 'light' ? 'dark' : 'light';
+      const nextTheme: AppTheme =
+        theme === AppThemeNames.light ? AppThemeNames.dark : AppThemeNames.light;
 
       return nextTheme;
     });
