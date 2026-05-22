923dea8a652b806eec7e48d8dca85958276aa099
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-22 14:03:46 +0700

    refactor(i18n): remove constructor and use defauld activeLang from service

--- src/app/core/services/transloco.service.ts
+++ src/app/core/services/transloco.service.ts
@@ -6,22 +6,17 @@ import { TranslocoService } from '@jsverse/transloco';
 })
 export class LanguageSwitcher {
   private translocoService = inject(TranslocoService);
-  public currentLanguage: string;
-  public languages: string[];
 
-  constructor() {
-    const availableLangs = this.translocoService.getAvailableLangs();
-
-    this.currentLanguage = this.translocoService.getDefaultLang();
-    this.languages = availableLangs.map((lang) => (typeof lang === 'string' ? lang : lang.id));
-  }
+  private availableLangs = this.translocoService.getAvailableLangs();
+  protected languages = this.availableLangs.map((lang) =>
+    typeof lang === 'string' ? lang : lang.id,
+  );
 
   public switchLanguage(lang: string) {
     this.translocoService.setActiveLang(lang);
-    this.currentLanguage = lang;
   }
   public languageSwitch() {
-    if (this.currentLanguage === 'en') {
+    if (this.translocoService.activeLang() === 'en') {
       this.switchLanguage('ru');
     } else {
       this.switchLanguage('en');
