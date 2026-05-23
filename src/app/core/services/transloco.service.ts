77d6ef74dca692449b5c2360e0589203e3428b8f
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-24 03:36:45 +0700

    feat(i18n): use local storage for keep current language

--- src/app/core/services/transloco.service.ts
+++ src/app/core/services/transloco.service.ts
@@ -1,12 +1,18 @@
 import { inject, Injectable } from '@angular/core';
 import { TranslocoService } from '@jsverse/transloco';
+import { effect } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LanguageSwitcher {
   private translocoService = inject(TranslocoService);
+  private savedLang = localStorage.getItem('lang') ?? this.translocoService.getDefaultLang();
+  private readonly changeToSavedLang = this.switchLanguage(this.savedLang);
   private readonly currentLanguage = this.translocoService.activeLang;
+  private readonly langEffect = effect(() => {
+    localStorage.setItem('lang', this.currentLanguage());
+  });
 
   private availableLangs = this.translocoService.getAvailableLangs();
   protected languages = this.availableLangs.map((lang) => {
