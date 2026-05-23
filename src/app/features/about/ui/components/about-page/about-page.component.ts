e9344dbc4aa891ad7a67f1c146ce798e9846b97e
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-24 00:51:22 +0700

    refactor(i18n): remove KeyValuePipe from about page

--- src/app/features/about/ui/components/about-page/about-page.component.ts
+++ src/app/features/about/ui/components/about-page/about-page.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { KeyValuePipe } from '@angular/common';
 import { TuiAccordion } from '@taiga-ui/kit';
 import { accordionData } from './constants/accordion-constants';
 import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'left-paw-about-page',
-  imports: [KeyValuePipe, TuiAccordion, TranslocoDirective],
+  imports: [TuiAccordion, TranslocoDirective],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
