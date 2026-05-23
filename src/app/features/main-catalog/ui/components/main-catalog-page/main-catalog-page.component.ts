93898fbd35f51ef23beb0e1008992bda01843084
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-23 12:35:03 +0300

    feat: add basic main catalog page components

--- src/app/features/main-catalog/ui/components/main-catalog-page/main-catalog-page.component.ts
+++ src/app/features/main-catalog/ui/components/main-catalog-page/main-catalog-page.component.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CatalogFilterComponent } from '../filter/catalog-filter/catalog-filter.component';
+import { CatalogPaginationComponent } from '../pagination/catalog-pagination/catalog-pagination.component';
+import { PokemonCardComponent } from '../pokemon-card/pokemon-card/pokemon-card.component';
+import { CatalogPageMocks } from './constants/catalog-page-mocks';
 
 @Component({
   selector: 'left-paw-main-catalog-page',
-  imports: [],
+  imports: [CatalogFilterComponent, CatalogPaginationComponent, PokemonCardComponent],
   templateUrl: './main-catalog-page.component.html',
   styleUrl: './main-catalog-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainCatalogPageComponent {}
+export class MainCatalogPageComponent {
+  protected readonly catalogPageMocks = CatalogPageMocks;
+}
