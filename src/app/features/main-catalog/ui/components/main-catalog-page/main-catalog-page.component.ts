795c2174f995b54b24b0826168255205a7040764
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-23 10:21:39 +0300

    feat: add basic main-catalog page/main-catalog-page

---
+++ src/app/features/main-catalog/ui/components/main-catalog-page/main-catalog-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'left-paw-main-catalog-page',
+  imports: [],
+  templateUrl: './main-catalog-page.component.html',
+  styleUrl: './main-catalog-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainCatalogPageComponent {}
