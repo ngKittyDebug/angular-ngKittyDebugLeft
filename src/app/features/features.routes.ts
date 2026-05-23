a6ee312e2fafd5a90a59b18c9ecd031ee9401eb9
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-23 10:51:53 +0300

    feat: add routing to the main catalog page

--- src/app/features/features.routes.ts
+++ src/app/features/features.routes.ts
@@ -1,5 +1,6 @@
 import type { Routes } from '@angular/router';
 import { aboutRoutes } from './about/about.routes';
 import { notFoundRoutes } from './not-found/not-found.routes';
+import { mainCatalogRoutes } from './main-catalog/main-catalog.routes';
 
-export const ChildrenRouts: Routes = [...aboutRoutes, ...notFoundRoutes];
+export const ChildrenRouts: Routes = [...mainCatalogRoutes, ...aboutRoutes, ...notFoundRoutes];
