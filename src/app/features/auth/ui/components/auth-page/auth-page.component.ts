4460d3d0b9b3c8a29c1172eb972d918cd362aa34
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-25 19:18:47 +0300

    fix: fix mistakes acording code review

--- src/app/features/auth/ui/components/auth-page/auth-page.component.ts
+++ src/app/features/auth/ui/components/auth-page/auth-page.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslocoPipe } from '@jsverse/transloco';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 const POKEMON_ICON_SRC = 'images/svg/pokeball.svg';
 
 @Component({
   selector: 'left-paw-auth-page',
-  imports: [RouterOutlet, TranslocoPipe],
+  imports: [RouterOutlet, TranslocoDirective],
   templateUrl: './auth-page.component.html',
   styleUrl: './auth-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
