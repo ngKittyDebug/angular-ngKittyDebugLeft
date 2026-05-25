2e091d6d755516e7a2fdb156c1b24c4af44b1911
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-26 01:55:23 +0700

    style(about): remove commented out code

--- src/app/features/about/ui/components/about-page/about-page.component.ts
+++ src/app/features/about/ui/components/about-page/about-page.component.ts
@@ -2,11 +2,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiAccordion } from '@taiga-ui/kit';
 import { accordionData } from './constants/accordion-constants';
 import { TranslocoDirective } from '@jsverse/transloco';
-import { TuiBadge, TuiStatus } from '@taiga-ui/kit';
 
 @Component({
   selector: 'left-paw-about-page',
-  imports: [TuiAccordion, TranslocoDirective, TuiBadge, TuiStatus],
+  imports: [TuiAccordion, TranslocoDirective],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
