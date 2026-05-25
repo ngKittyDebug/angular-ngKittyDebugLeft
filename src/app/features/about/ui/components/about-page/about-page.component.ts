4a017f313c9719bea9aec4c64942a2cb5a84acc5
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-26 01:46:33 +0700

    refactor(about): update style on about page

--- src/app/features/about/ui/components/about-page/about-page.component.ts
+++ src/app/features/about/ui/components/about-page/about-page.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiAccordion } from '@taiga-ui/kit';
 import { accordionData } from './constants/accordion-constants';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { TuiBadge, TuiStatus } from '@taiga-ui/kit';
 
 @Component({
   selector: 'left-paw-about-page',
-  imports: [TuiAccordion, TranslocoDirective],
+  imports: [TuiAccordion, TranslocoDirective, TuiBadge, TuiStatus],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
