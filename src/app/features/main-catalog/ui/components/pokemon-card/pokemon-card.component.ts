8283a0ba166e3df4573251af843c952195e72197
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-27 13:55:45 +0300

    feat: add pokemon card stats. add some solor constants

--- src/app/features/main-catalog/ui/components/pokemon-card/pokemon-card.component.ts
+++ src/app/features/main-catalog/ui/components/pokemon-card/pokemon-card.component.ts
@@ -1,10 +1,11 @@
 import type { OnInit } from '@angular/core';
-import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, signal } from '@angular/core';
 import type { PokemonDetailApiData } from '@shared/models/pokemon-detail-api-data-interface';
+import { TuiProgress } from '@taiga-ui/kit';
 
 @Component({
   selector: 'left-paw-pokemon-card',
-  imports: [],
+  imports: [TuiProgress],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -12,6 +13,11 @@ import type { PokemonDetailApiData } from '@shared/models/pokemon-detail-api-dat
 export class PokemonCardComponent implements OnInit {
   public readonly pokemonName = input<string>('pokemon');
   protected readonly pokemonCardData = signal<PokemonDetailApiData | null>(null);
+  protected readonly pokemonLimitedStats = computed(() => {
+    const data = this.pokemonCardData();
+
+    return data?.stats?.slice(0, 3) ?? [];
+  });
 
   public async ngOnInit() {
     console.log('boom');
@@ -26,7 +32,11 @@ export class PokemonCardComponent implements OnInit {
 
       this.pokemonCardData.set(result);
       console.log(this.pokemonCardData());
-      console.log(this.pokemonCardData()?.types);
+      console.log(this.pokemonLimitedStats());
+      // console.log(this.pokemonCardData()?.stats);
+      // console.log(this.pokemonCardData()?.stats[0]);
+      // console.log(this.pokemonCardData()?.stats[0].base_stat);
+      // console.log(this.pokemonCardData()?.stats[0].stat.name);
     } catch (error) {
       console.error('Ошибка при загрузке:', error);
     }
