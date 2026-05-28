57e2be79671b6efcde8d7cc8824ba1c6e8a8e36c
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-28 16:14:32 +0300

    feat: add basic pokemon profile component

---
+++ src/app/features/pokemon-profile/ui/components/pokemon-profile-page/pokemon-profile-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'left-paw-pokemon-profile-page',
+  imports: [],
+  templateUrl: './pokemon-profile-page.component.html',
+  styleUrl: './pokemon-profile-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PokemonProfilePageComponent {}
