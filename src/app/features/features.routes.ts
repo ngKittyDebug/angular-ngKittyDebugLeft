3b3cb862c98f5d850270a7fe8cd296a3cab843f3
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-28 17:08:47 +0300

    feat: add routing and tralations dictionary

--- src/app/features/features.routes.ts
+++ src/app/features/features.routes.ts
@@ -3,10 +3,12 @@ import { aboutRoutes } from './about/about.routes';
 import { notFoundRoutes } from './not-found/not-found.routes';
 import { authRoutes } from './auth/auth.routes';
 import { mainCatalogRoutes } from './main-catalog/main-catalog.routes';
+import { pokemonProfileRoutes } from './pokemon-profile/pokemon-profile.routes';
 
 export const ChildrenRouts: Routes = [
   ...mainCatalogRoutes,
   ...authRoutes,
   ...aboutRoutes,
+  ...pokemonProfileRoutes,
   ...notFoundRoutes,
 ];
