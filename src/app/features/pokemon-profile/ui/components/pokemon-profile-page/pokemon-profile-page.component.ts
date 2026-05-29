acfdd1e6e662fdb4997ed3cc5392a31fa1f0d23d
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-05-29 19:49:32 +0300

    feat: add some components to the pokemon profile page

--- src/app/features/pokemon-profile/ui/components/pokemon-profile-page/pokemon-profile-page.component.ts
+++ src/app/features/pokemon-profile/ui/components/pokemon-profile-page/pokemon-profile-page.component.ts
@@ -1,10 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import type { PokemonDetailApiData } from '@shared/models/pokemon-detail-api-data-interface';
+import type { PokemonSpeciesApiData } from '@shared/models/pokemon-species-api-data-interface';
+import { TuiBadge } from '@taiga-ui/kit';
 
 @Component({
   selector: 'left-paw-pokemon-profile-page',
-  imports: [],
+  imports: [TuiBadge],
   templateUrl: './pokemon-profile-page.component.html',
   styleUrl: './pokemon-profile-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PokemonProfilePageComponent {}
+export class PokemonProfilePageComponent implements OnInit {
+  public readonly pokemonEndpoint = 'bulbasaur';
+  protected readonly pokemonProfileData = signal<PokemonDetailApiData | null>(null);
+  protected readonly pokemonProfileDataSpecies = signal<PokemonSpeciesApiData | null>(null);
+
+  public async ngOnInit() {
+    try {
+      const response = await fetch(`/mocks/${this.pokemonEndpoint}.json`);
+      const responeSpecies = await fetch(`/mocks/${this.pokemonEndpoint}-species.json`);
+
+      if (!response.ok || !responeSpecies.ok) {
+        throw new Error('Ошибка сети');
+      }
+
+      const result: PokemonDetailApiData = (await response.json()) as PokemonDetailApiData;
+      const resultSpecies: PokemonSpeciesApiData =
+        (await responeSpecies.json()) as PokemonSpeciesApiData;
+
+      this.pokemonProfileData.set(result);
+      this.pokemonProfileDataSpecies.set(resultSpecies);
+
+      console.log(this.pokemonProfileData());
+      console.log(this.pokemonProfileDataSpecies());
+    } catch (error) {
+      console.error('Ошибка при загрузке:', error);
+    }
+  }
+}
