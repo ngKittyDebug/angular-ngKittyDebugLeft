fe799322fc8ac5c395a5874977fc8396f13b4def
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-05-31 06:22:18 +0700

    refactor(directive): use css class instead of inline styles

--- src/app/shared/directives/hide-content.directive.ts
+++ src/app/shared/directives/hide-content.directive.ts
@@ -1,33 +1,24 @@
-import { afterNextRender, Directive, ElementRef, inject, type OnDestroy } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: '[leftPawHideContent]',
+  host: {
+    '(window:scroll)': 'scrollLogic()',
+  },
 })
-export class HideContentDirective implements OnDestroy {
+export class HideContentDirective {
   private previousScrollTop = 0;
   private navItem = inject(ElementRef<HTMLElement>);
-  protected navHeight = '';
+  public readonly hideClass = input.required<string>();
 
-  constructor() {
-    afterNextRender(() => {
-      this.navHeight = window.getComputedStyle(this.navItem.nativeElement).height;
-    });
-    document.addEventListener('scroll', this.scrollLogic);
-  }
-
-  public ngOnDestroy() {
-    document.removeEventListener('scroll', this.scrollLogic);
-  }
-
-  private scrollLogic = () => {
+  protected scrollLogic() {
     const scrollTop = window.scrollY;
 
     if (scrollTop > this.previousScrollTop) {
-      this.navItem.nativeElement.style.height = '0';
+      this.navItem.nativeElement.classList.add(this.hideClass());
     } else {
-      this.navItem.nativeElement.style.height = this.navHeight;
+      this.navItem.nativeElement.classList.remove(this.hideClass());
     }
-
     this.previousScrollTop = scrollTop;
-  };
+  }
 }
