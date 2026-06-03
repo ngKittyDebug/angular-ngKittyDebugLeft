3a9656c09affa23373f6290a9619209c0e0d0235
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-06-03 20:48:32 +0700

    feat(profile): refine profile page layout after review

--- src/app/features/profile/ui/components/profile/profile.component.ts
+++ src/app/features/profile/ui/components/profile/profile.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiAvatar } from '@taiga-ui/kit';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'left-paw-profile',
-  imports: [],
+  imports: [TuiAvatar, TranslocoDirective],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  protected readonly sizes = ['xxl', 'xl', 'l', 'm', 's', 'xs'] as const;
+}
