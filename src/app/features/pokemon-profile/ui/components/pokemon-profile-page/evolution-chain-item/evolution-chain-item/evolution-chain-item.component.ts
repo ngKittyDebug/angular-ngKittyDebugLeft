8d5b47c428a7054c357cf4889bff3456ff1a9466
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-06-04 23:08:41 +0300

    fix: fix mistakes acording code review in profile page component

--- src/app/features/pokemon-profile/ui/components/pokemon-profile-page/evolution-chain-item/evolution-chain-item/evolution-chain-item.component.ts
+++ src/app/features/pokemon-profile/ui/components/pokemon-profile-page/evolution-chain-item/evolution-chain-item/evolution-chain-item.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
-import type { MyEvolutionNode } from '../../pokemon-profile-page/pokemon-profile-page.component';
 import { TuiButton, TuiCarousel, TuiIcon } from '@taiga-ui/core';
 import { TuiProgress } from '@taiga-ui/kit';
 import { TuiCard } from '@taiga-ui/layout';
+import type { EvolutionNodeModel } from '../../pokemon-profile-page.component';
 
 @Component({
   selector: 'left-paw-evolution-chain-item',
@@ -12,7 +12,7 @@ import { TuiCard } from '@taiga-ui/layout';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EvolutionChainItemComponent {
-  public readonly evolutionChain = input.required<MyEvolutionNode | null | undefined>();
+  public readonly evolutionChain = input.required<EvolutionNodeModel | null | undefined>();
   public readonly activeName = input<string | undefined>('');
   protected readonly index = signal(0);
 }
--- src/app/features/pokemon-profile/ui/components/pokemon-profile-page/pokemon-profile-page.component.ts
+++ src/app/features/pokemon-profile/ui/components/pokemon-profile-page/pokemon-profile-page.component.ts
@@ -8,16 +8,16 @@ import type {
   EvolutionChainItem,
   EvolutionChainResponse,
 } from '@shared/models/pokemon-evolution-chain-api-data-interface';
-import { EvolutionChainItemComponent } from '../evolution-chain-item/evolution-chain-item/evolution-chain-item.component';
-import { PokemonProfileInfoComponent } from '../pokemon-profile-info/pokemon-profile-info/pokemon-profile-info.component';
-import { PokemonProfileStatsComponent } from '../pokemon-profile-stats/pokemon-profile-stats/pokemon-profile-stats.component';
-import { PokemonProfileSpeciesBreedingComponent } from '../pokemon-profile-species-breeding/pokemon-profile-species-breeding/pokemon-profile-species-breeding.component';
+import { EvolutionChainItemComponent } from './evolution-chain-item/evolution-chain-item/evolution-chain-item.component';
+import { PokemonProfileInfoComponent } from './pokemon-profile-info/pokemon-profile-info/pokemon-profile-info.component';
+import { PokemonProfileStatsComponent } from './pokemon-profile-stats/pokemon-profile-stats/pokemon-profile-stats.component';
+import { PokemonProfileSpeciesBreedingComponent } from './pokemon-profile-species-breeding/pokemon-profile-species-breeding.component';
 
-export interface MyEvolutionNode {
+export interface EvolutionNodeModel {
   name: string;
   image: string;
   condition: string | null;
-  children: MyEvolutionNode[];
+  children: EvolutionNodeModel[];
 }
 
 @Component({
@@ -44,10 +44,9 @@ export class PokemonProfilePageComponent implements OnInit {
     return this.pokemonProfileData()?.stats.reduce((sum, entry) => sum + (entry.base_stat ?? 0), 0);
   });
   protected readonly pokemonEvolutionChain = computed(() => {
-    const data = this.pokemonProfileEvolutionChainData()?.chain as EvolutionChainItem;
-    const result = this.buildStructure(data);
+    const data = this.pokemonProfileEvolutionChainData()?.chain;
 
-    return result;
+    return data ? this.buildStructure(data) : null;
   });
 
   public async ngOnInit() {
@@ -74,17 +73,17 @@ export class PokemonProfilePageComponent implements OnInit {
     }
   }
 
-  private buildStructure(node: EvolutionChainItem): MyEvolutionNode | null {
+  private buildStructure(node: EvolutionChainItem): EvolutionNodeModel | null {
     if (!node || !node.species) {
-      return null; // ← Главная защита
+      return null;
     }
 
     const detail = node.evolution_details?.[0];
     const condition = detail?.min_level ? `Lv. ${detail.min_level}` : detail?.trigger?.name || null;
 
-    const children: MyEvolutionNode[] = (node.evolves_to || [])
+    const children: EvolutionNodeModel[] = (node.evolves_to || [])
       .map((child: EvolutionChainItem) => this.buildStructure(child))
-      .filter((c): c is MyEvolutionNode => c !== null);
+      .filter((c): c is EvolutionNodeModel => c !== null);
 
     return {
       name: node.species.name,
