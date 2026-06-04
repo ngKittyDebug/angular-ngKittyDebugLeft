cdaa587a0674cc01f8a8f4be1afe4a6dc8688df1
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-06-05 00:37:41 +0300

    fix: fix mistakes acordiing code review in the evolution chain component

--- src/app/features/pokemon-profile/ui/components/pokemon-profile-page/evolution-chain-item/evolution-chain-item/evolution-chain-item.component.ts
+++ src/app/features/pokemon-profile/ui/components/pokemon-profile-page/evolution-chain-item/evolution-chain-item/evolution-chain-item.component.ts
@@ -1,18 +1,17 @@
 import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
 import { TuiButton, TuiCarousel, TuiIcon } from '@taiga-ui/core';
-import { TuiProgress } from '@taiga-ui/kit';
 import { TuiCard } from '@taiga-ui/layout';
 import type { EvolutionNodeModel } from '../../pokemon-profile-page.component';
 
 @Component({
   selector: 'left-paw-evolution-chain-item',
-  imports: [TuiCard, TuiCarousel, TuiProgress, TuiButton, TuiIcon],
+  imports: [TuiCard, TuiCarousel, TuiButton, TuiIcon],
   templateUrl: './evolution-chain-item.component.html',
   styleUrl: './evolution-chain-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EvolutionChainItemComponent {
-  public readonly evolutionChain = input.required<EvolutionNodeModel | null | undefined>();
+  public readonly evolutionChain = input.required<EvolutionNodeModel>();
   public readonly activeName = input<string | undefined>('');
   protected readonly index = signal(0);
 }
