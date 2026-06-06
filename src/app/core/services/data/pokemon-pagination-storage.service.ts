8feafb64ba1e97a8c01310e044c1bd0410a94dce
AlexGorSer <taskekaminari@gmail.com>
2026-06-06 16:41:26 +0200

    refactor(service): separate pokemon-pagination-service

---
+++ src/app/core/services/data/pokemon-pagination-storage.service.ts
@@ -0,0 +1,13 @@
+import { inject, resource, Service } from '@angular/core';
+import { PokemonApiService } from '@core/api/pokemon-pagination-api.service';
+import type { PokemonListApiData } from '@shared/models/pokemon-list-api-data-interface';
+
+@Service()
+export class PokemonPaginationStorageService {
+  private readonly pokemonApiService = inject(PokemonApiService);
+
+  public readonly _pokemonPagination = resource({
+    loader: (): Promise<PokemonListApiData> =>
+      this.pokemonApiService.getPokemonPaginationData().then((data) => data.json()),
+  });
+}
--- src/app/core/services/pokemon/pokemon-pagination.service.ts
+++ src/app/core/services/pokemon/pokemon-pagination.service.ts
@@ -1,29 +1,13 @@
-import { computed, inject, resource, Service, signal } from '@angular/core';
-import { PokemonApiService } from '@core/api/pokemon-pagination-api.service';
+import { computed, inject, Service, signal } from '@angular/core';
 import { INITIAL_LIMIT_STEP } from '@core/constants/pokemon-constants';
-import type { PokemonListApiData } from '@shared/models/pokemon-list-api-data-interface';
+import { PokemonPaginationStorageService } from '../data/pokemon-pagination-storage.service';
 
 @Service()
 export class PokemonPaginationService {
-  private readonly pokemonApiService = inject(PokemonApiService);
-
-  private readonly _pokemonPagination = resource({
-    loader: (): Promise<PokemonListApiData> =>
-      this.pokemonApiService.getPokemonPaginationData().then((data) => data.json()),
-  });
+  private readonly pokemonPaginationStorageService = inject(PokemonPaginationStorageService);
+  private readonly _pokemonPagination = this.pokemonPaginationStorageService._pokemonPagination;
 
   public readonly currentPage = signal<number>(0);
-
-  public readonly isLoadingPokemonPaginationData = this._pokemonPagination.asReadonly().isLoading;
-
-  public readonly currentPokemonDataAfterPagination = computed(() => {
-    return this.pokemonPaginationData()?.results.slice(
-      this.currentPage() * INITIAL_LIMIT_STEP,
-      this.currentPage() * INITIAL_LIMIT_STEP + INITIAL_LIMIT_STEP,
-    );
-  });
-
-  public readonly pokemonPaginationData = this._pokemonPagination.asReadonly().value;
 
   public readonly countPokemonData = computed(() => this.pokemonPaginationData()?.count);
 
@@ -31,6 +15,17 @@ export class PokemonPaginationService {
     const count = this.countPokemonData() || 0;
 
     return Math.floor(count / INITIAL_LIMIT_STEP);
+  });
+
+  public readonly isLoadingPokemonPaginationData = this._pokemonPagination.asReadonly().isLoading;
+
+  public readonly pokemonPaginationData = this._pokemonPagination.asReadonly().value;
+
+  public readonly currentPokemonDataAfterPagination = computed(() => {
+    return this.pokemonPaginationData()?.results.slice(
+      this.currentPage() * INITIAL_LIMIT_STEP,
+      this.currentPage() * INITIAL_LIMIT_STEP + INITIAL_LIMIT_STEP,
+    );
   });
 
   public increasePaginationCount() {
