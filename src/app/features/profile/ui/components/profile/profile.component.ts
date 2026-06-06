9524554f0380f543163343e7e3636087cd82e3da
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-06-06 20:47:25 +0700

    feat(profile): add icon for achievements

--- src/app/features/profile/ui/components/profile/profile.component.ts
+++ src/app/features/profile/ui/components/profile/profile.component.ts
@@ -3,15 +3,19 @@ import { TuiAvatar } from '@taiga-ui/kit';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { CatalogPageMocks } from '@features/main-catalog/ui/components/main-catalog-page/constants/catalog-page-mocks';
 import { PokemonCardProfileComponent } from '../pokemon-card-profile/pokemon-card-profile.component';
+import { TuiIcon } from '@taiga-ui/core';
 
 @Component({
   selector: 'left-paw-profile',
-  imports: [TuiAvatar, TranslocoDirective, PokemonCardProfileComponent],
+  imports: [TuiAvatar, TranslocoDirective, PokemonCardProfileComponent, TuiIcon],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent {
+  public achievements: string[] = ['exempleLong', 'exemple', 'exemple', 'exempleLong'];
+  // TODO сейчас используется масив звтычка, чтобы посмотреть как будет отробатывать прорисовка
+
   protected readonly sizes = ['xxl', 'xl', 'l', 'm', 's', 'xs'] as const;
   protected readonly favoritesList = CatalogPageMocks;
   protected readonly caughtList = CatalogPageMocks;
