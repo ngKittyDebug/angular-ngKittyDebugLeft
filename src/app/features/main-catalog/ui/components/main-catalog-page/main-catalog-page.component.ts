604c2899753f9d7e77e896f88d88d1dd294b7643
AlexGorSer <taskekaminari@gmail.com>
2026-06-07 02:03:26 +0200

    feat(main): add tui-loader

--- src/app/features/main-catalog/ui/components/main-catalog-page/main-catalog-page.component.ts
+++ src/app/features/main-catalog/ui/components/main-catalog-page/main-catalog-page.component.ts
@@ -3,13 +3,15 @@ import { CatalogFilterComponent } from '../filter/catalog-filter/catalog-filter.
 import { CatalogPaginationComponent } from '../pagination/catalog-pagination/catalog-pagination.component';
 import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
 import { PokemonPaginationService } from '@core/services/pokemon/pokemon-pagination.service';
+import { TuiLoader, tuiLoaderOptionsProvider } from '@taiga-ui/core';
 
 @Component({
   selector: 'left-paw-main-catalog-page',
-  imports: [CatalogFilterComponent, CatalogPaginationComponent, PokemonCardComponent],
+  imports: [CatalogFilterComponent, CatalogPaginationComponent, PokemonCardComponent, TuiLoader],
   templateUrl: './main-catalog-page.component.html',
   styleUrl: './main-catalog-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [tuiLoaderOptionsProvider({ size: 'xxl' })],
 })
 export class MainCatalogPageComponent {
   protected pokemonPaginationService = inject(PokemonPaginationService);
