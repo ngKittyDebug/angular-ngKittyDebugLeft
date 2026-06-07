c75801142a78e0c5af76ef0573732d10166dec12
AlexGorSer <taskekaminari@gmail.com>
2026-06-07 16:08:54 +0200

    fix(main): remove unused provider class

--- src/app/features/main-catalog/main-catalog.routes.ts
+++ src/app/features/main-catalog/main-catalog.routes.ts
@@ -3,7 +3,6 @@ import { authGuard } from '@shared/guards/auth.guard';
 import { MainCatalogFacade } from './data/facades/main-catalog.facade';
 import { PokemonPaginationStorageService } from './data/services/pokemon-pagination-storage.service';
 import { PokemonPaginationService } from './data/services/pokemon-pagination.service';
-import { PokemonStorageService } from './data/services/pokemon-storage.service';
 
 export const mainCatalogRoutes: Routes = [
   {
@@ -12,12 +11,7 @@ export const mainCatalogRoutes: Routes = [
       import('./ui/components/main-catalog-page/main-catalog-page.component').then(
         (m) => m.MainCatalogPageComponent,
       ),
-    providers: [
-      MainCatalogFacade,
-      PokemonStorageService,
-      PokemonPaginationService,
-      PokemonPaginationStorageService,
-    ],
+    providers: [MainCatalogFacade, PokemonPaginationService, PokemonPaginationStorageService],
     canActivate: [authGuard],
   },
 ];
