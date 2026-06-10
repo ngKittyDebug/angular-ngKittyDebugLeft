b245f246c8b7ee195aefc884cd9e338bf693c8b6
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-06-10 21:10:17 +0700

    refactor(navigation): bind routerLink correctly

--- src/app/core/ui/components/layout/constants/nav-list.ts
+++ src/app/core/ui/components/layout/constants/nav-list.ts
@@ -1,22 +1,32 @@
+import { aboutRoutes } from '@features/about/about.routes';
+import { mainCatalogRoutes } from '@features/main-catalog/main-catalog.routes';
+import { profileRoutes } from '@features/profile/profile.routes';
+import { frenzyRoutes } from '@features/frenzy/frenzy.routes';
+
 export const NAV_LIST_ITEMS = [
   {
-    label: 'header.catalog',
-    icon: '@tui.panel-top-bottom-dashed',
+    label: 'header.pokemon',
+    icon: '@tui.paw-print',
     link: '#',
   },
   {
-    label: 'header.pokemon',
-    icon: '@tui.sparkles',
-    link: '#',
+    label: 'header.frenzy',
+    icon: '@tui.gamepad-2',
+    link: `/${frenzyRoutes[0].path}`,
+  },
+  {
+    label: 'header.catalog',
+    icon: '@tui.panel-top-bottom-dashed',
+    link: `/${mainCatalogRoutes[0].path}`,
   },
   {
     label: 'header.profile',
     icon: '@tui.user',
-    link: '#',
+    link: `/${profileRoutes[0].path}`,
   },
   {
-    label: 'header.settings',
-    icon: '@tui.settings',
-    link: '#',
+    label: 'header.about',
+    icon: '@tui.heart-handshake',
+    link: `/${aboutRoutes[0].path}`,
   },
 ];
