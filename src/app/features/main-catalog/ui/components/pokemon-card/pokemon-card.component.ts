47f1994854679b07443bf4defc89d09f09ec709a
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-06-14 10:30:58 +0300

    feat: add basic sceleton to the pocemon card component

--- src/app/features/main-catalog/ui/components/pokemon-card/pokemon-card.component.ts
+++ src/app/features/main-catalog/ui/components/pokemon-card/pokemon-card.component.ts
@@ -4,11 +4,11 @@ import type { OnInit } from '@angular/core';
 import { ChangeDetectionStrategy, Component, computed, inject, input, signal } from '@angular/core';
 import { POKEMON_BASE_API } from '@core/constants/pokemon-constants';
 import type { PokemonDetailApiData } from '@shared/models/pokemon-detail-api-data-interface';
-import { TuiBadge, TuiProgress } from '@taiga-ui/kit';
+import { TuiBadge, TuiProgress, TuiSkeleton } from '@taiga-ui/kit';
 
 @Component({
   selector: 'left-paw-pokemon-card',
-  imports: [TuiProgress, TuiBadge, TitleCasePipe],
+  imports: [TuiProgress, TuiSkeleton, TuiBadge, TitleCasePipe],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
