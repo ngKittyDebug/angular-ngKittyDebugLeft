f41f267af547e2f724c18ed419903b4932894c54
AlexGorSer <taskekaminari@gmail.com>
2026-06-16 00:09:26 +0200

    feat(core): add url token, add base auth-api service

--- src/app/core/constants/pokemon-constants.ts
+++ src/app/core/constants/pokemon-constants.ts
@@ -1,1 +1,3 @@
 export const POKEMON_BASE_API = 'https://pokeapi.co/api/v2/';
+
+export const AUTH_SERVER_URL = 'https://meow-vault-pr-216.onrender.com/';
---
+++ src/app/core/tokens/auth-server-url.token.ts
@@ -0,0 +1,3 @@
+import { InjectionToken } from '@angular/core';
+
+export const AUTH_SERVER_URL_TOKEN = new InjectionToken<string>('Базовый url для сервера');
