88911350e43803545944814a22f0dff00885b873
AlexGorSer <taskekaminari@gmail.com>
2026-06-18 03:34:13 +0200

    fix(auth): separate email and username functions validators

--- src/app/features/auth/data/helpers/email-validator.ts
+++ src/app/features/auth/data/helpers/email-validator.ts
@@ -1,14 +1,11 @@
-import type { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import type { AbstractControl, ValidationErrors } from '@angular/forms';
 import { EMAIL_PATTERN } from '@shared/constants/patterns-constants';
 
-export function emailValidator(): ValidatorFn;
-export function emailValidator(email: string): boolean;
+export function isValidEmail(email: string): boolean {
+  return EMAIL_PATTERN.test(email);
+}
 
-export function emailValidator(email?: string) {
-  if (email) {
-    return EMAIL_PATTERN.test(email);
-  }
-
+export function emailValidator() {
   return (control: AbstractControl<string>): ValidationErrors | null => {
     const value = control.value;
 
--- src/app/features/auth/data/helpers/login-convert.ts
+++ src/app/features/auth/data/helpers/login-convert.ts
@@ -1,14 +1,14 @@
 import type { CredentialsApiData } from '../models/credentials-api-data';
 import type { LoginFormGroup } from '../models/login/login-form.model';
-import { emailValidator } from './email-validator';
-import { userNameValidator } from './username-validator';
+import { isValidEmail } from './email-validator';
+import { isValidUserName } from './username-validator';
 
 export const convertLoginFormModelToCredentialsApiData = (
   loginFormGroup: LoginFormGroup,
 ): CredentialsApiData => {
   return {
-    email: emailValidator(loginFormGroup.nameOrEmail.value) ? loginFormGroup.nameOrEmail.value : '',
-    username: userNameValidator(loginFormGroup.nameOrEmail.value)
+    email: isValidEmail(loginFormGroup.nameOrEmail.value) ? loginFormGroup.nameOrEmail.value : '',
+    username: isValidUserName(loginFormGroup.nameOrEmail.value)
       ? loginFormGroup.nameOrEmail.value
       : '',
     password: loginFormGroup.password.value,
