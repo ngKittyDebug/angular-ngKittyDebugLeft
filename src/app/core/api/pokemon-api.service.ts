66db63d11ecd81bd8d00b8e221b14daa73dfe3a7
AlexGorSer <taskekaminari@gmail.com>
2026-06-20 03:58:04 +0200

    feat(main): add foundByType rxResource

--- src/app/core/api/pokemon-api.service.ts
+++ src/app/core/api/pokemon-api.service.ts
@@ -1,10 +1,13 @@
-import { Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
 import { POKEMON_BASE_API } from '@core/constants/pokemon-constants';
 
 const ALL_POKEMON_LIMIT = 10000;
 
 @Service()
 export class PokemonApiService {
+  private readonly http = inject(HttpClient);
+
   public getPokemonPaginationUrl(options = { limitPokemon: ALL_POKEMON_LIMIT }): string {
     return `${POKEMON_BASE_API}pokemon?limit=${options.limitPokemon}`;
   }
@@ -20,11 +23,11 @@ export class PokemonApiService {
   public getEvolutionChain(pokemonEndpoint: string): string {
     return `${POKEMON_BASE_API}evolution-chain/${pokemonEndpoint}`;
   }
-  public async getTypeList(): Promise<Response> {
-    return fetch(`${POKEMON_BASE_API}type`);
+  public getTypeList<T>(type = '') {
+    return this.http.get<T>(`${POKEMON_BASE_API}type/${type}`);
   }
 
-  public async getGenerationList(): Promise<Response> {
-    return fetch(`${POKEMON_BASE_API}generation`);
+  public getGenerationList<T>(generation = '') {
+    return this.http.get<T>(`${POKEMON_BASE_API}generation/${generation}`);
   }
 }
--- src/app/features/main-catalog/data/services/pokemon-filter-storage.service.ts
+++ src/app/features/main-catalog/data/services/pokemon-filter-storage.service.ts
@@ -1,6 +1,18 @@
-import { computed, inject, resource, Service, signal } from '@angular/core';
+import { computed, effect, inject, Service, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { PokemonApiService } from '@core/api/pokemon-api.service';
-import type { PokemonListApiData } from '@shared/models/pokemon-list-api-data-interface';
+import type {
+  PokemonListApiData,
+  PokemonListItemApiData,
+} from '@shared/models/pokemon-list-api-data-interface';
+import { forkJoin, map, of } from 'rxjs';
+
+interface PokemonTypes {
+  id: string;
+  pokemon: {
+    pokemon: PokemonListItemApiData;
+  };
+}
 
 @Service({ autoProvided: false })
 export class PokemonFilterStorageService {
@@ -10,21 +22,52 @@ export class PokemonFilterStorageService {
   public readonly filterByTypes = signal<string[]>([]);
   public readonly filterByGenerations = signal<string[]>([]);
 
-  public readonly typeListResource = resource({
-    loader: (): Promise<string[]> =>
+  public readonly typeListResource = rxResource({
+    stream: () =>
       this.pokemonApiService
-        .getTypeList()
-        .then((r) => r.json() as Promise<PokemonListApiData>)
-        .then((data) => data.results.map((t) => t.name)),
+        .getTypeList<PokemonListApiData>()
+        .pipe(map((data) => data.results.map((t) => t.name))),
   });
 
-  public readonly generationListResource = resource({
-    loader: (): Promise<string[]> =>
+  public readonly generationListResource = rxResource({
+    stream: () =>
       this.pokemonApiService
-        .getGenerationList()
-        .then((r) => r.json() as Promise<PokemonListApiData>)
-        .then((data) => data.results.map((g) => g.name)),
+        .getGenerationList<PokemonListApiData>()
+        .pipe(map((data) => data.results.map((t) => t.name))),
   });
+
   public readonly typeList = computed(() => this.typeListResource.value());
   public readonly generationList = computed(() => this.generationListResource.value());
+
+  public readonly foundByType = rxResource({
+    params: () => this.filterByTypes(),
+    stream: ({ params }) => {
+      if (params.length === 0) {
+        return of([]);
+      }
+
+      const requests = params.map((type) =>
+        this.pokemonApiService.getTypeList<PokemonTypes>(type).pipe(map((data) => data.pokemon)),
+      );
+
+      return forkJoin(requests).pipe(
+        map((arrayOfArrays) => {
+          const flatList = arrayOfArrays.flat();
+
+          return flatList.filter(
+            (item, index, self) =>
+              self.findIndex((object) => object.pokemon.name === item.pokemon.name) === index,
+          );
+        }),
+      );
+    },
+  });
+
+  public readonly bla = computed(() => this.foundByType.value());
+
+  constructor() {
+    effect(() => {
+      console.log(this.bla());
+    });
+  }
 }
