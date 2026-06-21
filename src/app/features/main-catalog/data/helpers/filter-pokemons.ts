aa59fb9a44330735f55d53611d0fa5ae691c5d4e
AlexGorSer <taskekaminari@gmail.com>
2026-06-21 14:00:18 +0200

    fix(main): delete useless map()

--- src/app/features/main-catalog/data/helpers/filter-pokemons.ts
+++ src/app/features/main-catalog/data/helpers/filter-pokemons.ts
@@ -7,7 +7,7 @@ export function filterCommonPokemons(
     return [];
   }
   if (arrayOfArraysOfPokemon.length === 1) {
-    return arrayOfArraysOfPokemon[0].map((item) => item);
+    return arrayOfArraysOfPokemon[0];
   }
   const firstArray = arrayOfArraysOfPokemon[0];
 
@@ -15,9 +15,7 @@ export function filterCommonPokemons(
     .slice(1)
     .map((subArray) => new Set(subArray.map((item) => item.name)));
 
-  return firstArray
-    .filter((item) => otherArraysSets.every((nameSet) => nameSet.has(item.name)))
-    .map((item) => item);
+  return firstArray.filter((item) => otherArraysSets.every((nameSet) => nameSet.has(item.name)));
 }
 
 export function intersectNonEmpty(
