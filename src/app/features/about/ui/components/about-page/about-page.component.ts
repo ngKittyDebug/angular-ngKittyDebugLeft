f8493a3ba3245e89020608863c65a3fac8dbf1ef
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-07-24 01:57:46 +0700

    fix(about): add NgOptimizedImage, aria-label disclosure, accordionList field rename

--- src/app/features/about/ui/components/about-page/about-page.component.ts
+++ src/app/features/about/ui/components/about-page/about-page.component.ts
@@ -1,15 +1,16 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiAccordion } from '@taiga-ui/kit';
-import { ACCORDION_LIST } from './constants/accordion-constants';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { ACCORDION_LIST } from './constants/accordion-constants';
 
 @Component({
   selector: 'left-paw-about-page',
-  imports: [TuiAccordion, TranslocoDirective],
+  imports: [NgOptimizedImage, TranslocoDirective, TuiAccordion],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutPageComponent {
-  protected readonly ACCORDION_LIST = ACCORDION_LIST;
+  protected readonly accordionList = ACCORDION_LIST;
 }
